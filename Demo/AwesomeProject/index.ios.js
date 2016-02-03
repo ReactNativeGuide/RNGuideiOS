bcdb7b0e79e6f809d9f6c7e3d4c960cc32a2920a
Eric <eric@saick.net>
2016-02-03 11:07:50 +0800

    Fetching real data

--- Demo/AwesomeProject/index.ios.js
+++ Demo/AwesomeProject/index.ios.js
@@ -12,13 +12,75 @@ import React, {
   Image
 } from 'react-native';
 
-var MOCKED_MOVIES_DATA = [
-  {title: 'Title', year: '2015', posters: {thumbnail: 'http://i.imgur.com/UePbdph.jpg'}},
-];
+/**
+ * For quota reasons we replaced the Rotten Tomatoes' API with a sample data of
+ * their very own API that lives in React Native's Github repo.
+ */
+var REQUEST_URL = 'https://raw.githubusercontent.com/facebook/react-native/master/docs/MoviesExample.json';
+
+//var MOCKED_MOVIES_DATA = [
+//  {title: 'Title', year: '2015', posters: {thumbnail: 'http://i.imgur.com/UePbdph.jpg'}},
+//];
 
 class AwesomeProject extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      movies: null,
+    };
+  }
+
+  componentDidMount() {
+    this.fetchData();
+  }
+
+  fetchData() {
+    fetch(REQUEST_URL)
+      .then((response) => response.json())
+      .then((responseData) => {
+        this.setState({
+          movies: responseData.movies,
+        });
+      })
+      .done();
+  }
+
+  //render() {
+  //  var movie = MOCKED_MOVIES_DATA[0];
+  //  return (
+  //    <View style={styles.container}>
+  //      <Image
+  //        source={{uri: movie.posters.thumbnail}}
+  //        style={styles.thumbnail}
+  //      />
+  //      <View style={styles.rightContainer}>
+  //        <Text style={styles.title}>{movie.title}</Text>
+  //        <Text style={styles.year}>{movie.year}</Text>
+  //      </View>
+  //    </View>
+  //  );
+  //}
+
   render() {
-    var movie = MOCKED_MOVIES_DATA[0];
+    if (!this.state.movies) {
+      return this.renderLoadingView();
+    }
+
+    var movie = this.state.movies[0];
+    return this.renderMovie(movie);
+  }
+
+  renderLoadingView() {
+    return (
+      <View style={styles.container}>
+        <Text>
+          Loading movies...
+        </Text>
+      </View>
+    );
+  }
+
+  renderMovie(movie) {
     return (
       <View style={styles.container}>
         <Image
