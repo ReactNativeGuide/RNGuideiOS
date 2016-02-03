00596f966c11c56ef88dbfb53881632ff1795169
Eric <eric@saick.net>
2016-02-03 17:50:55 +0800

    Argument Types
    研究参数传递，只支持基础数据类型。
    像Date这样的格式需要转换

--- Demo/IntergratingDemo/ReactComponent/componentIndex.ios.js
+++ Demo/IntergratingDemo/ReactComponent/componentIndex.ios.js
@@ -2,25 +2,25 @@
 
 var React = require('react-native');
 var {
-    Text,
-    View
-} = React;
+  Text,
+  View
+  } = React;
 
 var styles = React.StyleSheet.create({
-                                     container: {
-                                     flex: 1,
-                                     backgroundColor: 'red'
-                                     }
-                                     });
+  container: {
+    flex: 1,
+    backgroundColor: 'red'
+  }
+});
 
 class SimpleApp extends React.Component {
-    render() {
-        return (
-                <View style={styles.container}>
-                <Text>This is a simple application.</Text>
-                </View>
-                )
-    }
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>This is a simple application.</Text>
+      </View>
+    )
+  }
 }
 
 React.AppRegistry.registerComponent('SimpleApp', () => SimpleApp);
@@ -28,4 +28,13 @@ React.AppRegistry.registerComponent('SimpleApp', () => SimpleApp);
 // React-Native Module Example
 
 var CalendarManager = require('react-native').NativeModules.CalendarManager;
-CalendarManager.addEvent('Birthday Party', '4 Privet Drive, Surrey');
+var date = new Date();
+//CalendarManager.addEvent('Birthday Party', '4 Privet Drive, Surrey');
+//CalendarManager.addEvent('Birthday Party', '4 Privet Drive, Surrey', date.getTime()); // passing date as number of seconds since Unix epoch
+//CalendarManager.addEvent('Birthday Party', '4 Privet Drive, Surrey', date.toISOString()); // passing date as ISO-8601 string
+
+CalendarManager.addEvent('Birthday Party', {
+  location: '4 Privet Drive, Surrey',
+  time: date.getTime(),
+  description: '...'
+})
--- Demo/IntergratingDemo/ReactModuleExample/CalendarManager.m
+++ Demo/IntergratingDemo/ReactModuleExample/CalendarManager.m
@@ -8,14 +8,41 @@
 
 #import "CalendarManager.h"
 #import "RCTLog.h"
+#import "RCTConvert.h"
 
 @implementation CalendarManager
 
 RCT_EXPORT_MODULE()
 
-RCT_EXPORT_METHOD(addEvent:(NSString *)name location:(NSString *)location)
+//RCT_EXPORT_METHOD(addEvent:(NSString *)name location:(NSString *)location)
+//{
+//    RCTLogInfo(@"Pretending to create an event %@ at %@", name, location);
+//}
+
+//RCT_EXPORT_METHOD(addEvent:(NSString *)name location:(NSString *)location date:(nonnull NSNumber *)secondsSinceUnixEpoch)
+//{
+//    NSDate *date = [RCTConvert NSDate:secondsSinceUnixEpoch];
+//    RCTLogInfo(@"Pretending to create an event %@ at %@ at:%@", name, location, date);
+//}
+
+//RCT_EXPORT_METHOD(addEvent:(NSString *)name location:(NSString *)location date:(NSString *)ISO8601DateString)
+//{
+//    NSDate *date = [RCTConvert NSDate:ISO8601DateString];
+//    RCTLogInfo(@"Pretending to create an event %@ at %@ at:%@", name, location, date);
+//}
+
+// for auto convert in the future
+//RCT_EXPORT_METHOD(addEvent:(NSString *)name location:(NSString *)location date:(NSDate *)date)
+//{
+//    // Date is ready to use!
+//}
+
+RCT_EXPORT_METHOD(addEvent:(NSString *)name details:(NSDictionary *)details)
 {
-    RCTLogInfo(@"Pretending to create an event %@ at %@", name, location);
+    NSString *location = [RCTConvert NSString:details[@"location"]];
+    NSDate *time = [RCTConvert NSDate:details[@"time"]];
+
+    RCTLogInfo(@"Pretending to create an event %@ at %@ at:%@", name, location, time);
 }
 
 @end
